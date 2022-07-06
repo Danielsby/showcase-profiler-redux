1eb2a5c8522d489d568b1bdb16367249a2c301a5
Daniel By <daniel.by@spk.no>
2022-07-06 15:30:35 +0200

    feature: added a basic profiler setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {Profiler, useState} from "react";
+import {Button, handleProfilerRender} from "./Components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+  const [value, setValue] = useState(0);
+
+  return (<div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo"/>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+
+      <Profiler id="Frontpage" onRender={handleProfilerRender}>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,9 +22,15 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
+
+        <Button onClickFunction={() => {
+          setValue(value + 1)
+        }}>
+          Click here
+        </Button>
+      </Profiler>
+    </header>
+  </div>);
 }
 
 export default App;
